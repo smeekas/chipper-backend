e40ffeea358d2d33c409751dcb3fc0121c42cb50
smeekas <smeetapatel008@gmail.com>
2023-09-20 19:47:43 +0530

    fixes

--- build/index.js
+++ build/index.js
@@ -12,11 +12,12 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 9999;
 // let fs = require("fs");
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 // Handling '/' Request
 app.get("/", (_req, _res) => {
     const rawData = fs_1.default.readFileSync("./db.json", "utf-8");
-    console.log(rawData);
-    console.log(JSON.parse(rawData));
+    // console.log(rawData);
+    // console.log(JSON.parse(rawData));
     _res.send(JSON.parse(rawData));
 });
 app.post("/", (_req, _res) => {
@@ -26,8 +27,17 @@ app.post("/", (_req, _res) => {
     jsonData.push(_req.body);
     fs_1.default.writeFileSync("./db.json", JSON.stringify(jsonData), "utf-8");
 });
+app.post("/sub", (req, res) => {
+    const rawData = fs_1.default.readFileSync("./sub.json", "utf-8");
+    const jsonData = JSON.parse(rawData);
+    console.log(req.body);
+});
 // Server setup
 app.listen(port, () => {
     console.log(`TypeScript with Express
          http://localhost:${port}/`);
 });
+// Public Key:
+// BFfDbXYABqkgeRUfkE5BrS484nEU9ZQ00MGYUk3ceVOtCdvIDU1E63lNJ3d8uraB4cbJQTZc3S7OxV0b78492qw
+// Private Key:
+// p_tXIna1_E_KFSPXW4WvFyL237Yo1M6KfMWIC-dSieg
--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const app: express.Application = express();
 const port = process.env.PORT || 9999;
 // let fs = require("fs");
 app.use(cors());
+app.use(express.json());
 // Handling '/' Request
 app.get("/", (_req, _res) => {
   const rawData = fs.readFileSync("./db.json", "utf-8");
@@ -23,8 +24,18 @@ app.post("/", (_req, _res) => {
   jsonData.push(_req.body);
   fs.writeFileSync("./db.json", JSON.stringify(jsonData), "utf-8");
 });
+app.post("/sub", (req, res) => {
+  const rawData = fs.readFileSync("./sub.json", "utf-8");
+  const jsonData = JSON.parse(rawData);
+  console.log(req.body);
+});
 // Server setup
 app.listen(port, () => {
   console.log(`TypeScript with Express
          http://localhost:${port}/`);
 });
+// Public Key:
+// BFfDbXYABqkgeRUfkE5BrS484nEU9ZQ00MGYUk3ceVOtCdvIDU1E63lNJ3d8uraB4cbJQTZc3S7OxV0b78492qw
+
+// Private Key:
+// p_tXIna1_E_KFSPXW4WvFyL237Yo1M6KfMWIC-dSieg
